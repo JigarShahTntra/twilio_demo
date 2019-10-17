02c70342997d05e6f803da9a5370847bb3249439
YOUR NAME <jigar.shah@botreetechnologies.com>
2019-10-17 10:44:01 +0530

    changes

--- lib/message_sender.rb
+++ lib/message_sender.rb
@@ -1,7 +1,7 @@
 module MessageSender
   def self.send_code(phone_number, code)
-    account_sid = 'ACff849d389306a22bf267475632a58ee1'
-    auth_token  = 'd98cb8ffb2dbf189c271bde9734604e3'
+    account_sid = 'SID'
+    auth_token  = 'token'
     client = Twilio::REST::Client.new(account_sid, auth_token)
 
     message = client.messages.create(
